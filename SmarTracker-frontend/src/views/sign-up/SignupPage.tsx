27209f174c9f5d5ac7cf6d212ca7b126db0f03c6
Alon <alongold91@gmail.com>
2025-03-03 22:40:27 +0200

    feat(frontend/signuppage): add more input fields and extend validation

--- SmarTracker-frontend/src/views/sign-up/SignupPage.tsx
+++ SmarTracker-frontend/src/views/sign-up/SignupPage.tsx
@@ -2,7 +2,7 @@ import classes from './Signup.module.css';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
-import { Button, Input } from 'antd';
+import { Button, Checkbox, Flex, Input, InputNumber, Select } from 'antd';
 // Define the validation schema with Zod
 const formSchema = z
   .object({
@@ -12,7 +12,12 @@ const formSchema = z
       .min(8, 'Password must be at least 8 characters')
       .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
       .regex(/[0-9]/, 'Password must contain at least one number'),
-    confirmPassword: z.string().min(1, 'Please confirm your password')
+    confirmPassword: z.string().min(1, 'Please confirm your password'),
+    homeCurrency: z.string().min(1, 'Please select a currency'),
+    interestedInWarningEmails: z.boolean(),
+    warningPercentage: z.number().refine((data) => data >= 0 && data <= 100, {
+      message: 'Warning percentage must be between 0 and 100'
+    })
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
@@ -24,6 +29,8 @@ type FormData = z.infer<typeof formSchema>;
 const SignupPage = () => {
   const {
     control,
+    getValues,
+    watch,
     handleSubmit,
     formState: { errors }
   } = useForm<FormData>({
@@ -31,7 +38,10 @@ const SignupPage = () => {
     defaultValues: {
       email: '',
       password: '',
-      confirmPassword: ''
+      confirmPassword: '',
+      homeCurrency: '',
+      interestedInWarningEmails: false,
+      warningPercentage: undefined
     }
   });
 
@@ -79,7 +89,6 @@ const SignupPage = () => {
             <div>
               <label>Confirm Password</label>
               <Input.Password
-                className={classes.test}
                 {...field}
                 placeholder='Confirm password'
                 status={errors.confirmPassword ? 'error' : ''}
@@ -90,6 +99,53 @@ const SignupPage = () => {
             </div>
           )}
         />
+        <Controller
+          name='homeCurrency'
+          control={control}
+          render={({ field }) => (
+            <div className={`${classes['full-row']} ${classes['select']}`}>
+              <label>Select Currency</label>
+              <Select {...field} placeholder='Select currency'>
+                <Select.Option value='USD'>USD</Select.Option>
+                <Select.Option value='EUR'>EUR</Select.Option>
+              </Select>
+              {errors.homeCurrency && <p>{errors.homeCurrency.message}</p>}
+            </div>
+          )}
+        />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Controller
+            name='interestedInWarningEmails'
+            control={control}
+            render={({ field }) => (
+              <div>
+                <label style={{ fontSize: '12px', marginRight: '5px' }}>
+                  Notify me when expenses reach a budget percentage{' '}
+                </label>
+                <Checkbox {...field} checked={getValues('interestedInWarningEmails')} />
+                {errors.interestedInWarningEmails && (
+                  <p>{errors.interestedInWarningEmails.message}</p>
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            name='warningPercentage'
+            control={control}
+            render={({field}) => (
+              <Flex align='center' gap='.5rem'>
+                <InputNumber
+                  {...field}
+                  disabled={!watch('interestedInWarningEmails')}
+                  style={{ width: '30%' }} //TODO: GIVE IT A CLASS
+                 
+                />
+                <p>%</p>
+                // TODO: ADD ERROR TEXT
+              </Flex>
+            )}
+          />
+        </div>
 
         <Button type='primary' htmlType='submit' block>
           Submit
